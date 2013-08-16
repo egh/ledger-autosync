f601f2f1447549f62b7146e6fe663b96b68c9f70
Erik Hetzner <egh@e6h.org>
2013-08-15 19:56:39 -0700

    add get_new_txns method to sync

--- ledgerautosync/sync.py
+++ ledgerautosync/sync.py
@@ -1,9 +1,31 @@
+from ofxparse import OfxParser
+from StringIO import StringIO
+
 class Synchronizer(object):
     def __init__(self, ledger):
         self.ledger = ledger
     
-    def is_transaction_synced(self, txn):
+    def is_txn_synced(self, txn):
         return (self.ledger.get_transaction("meta fid=%s"%(txn.id)) != None)
     
-    def sync(self, txns):
-        return [ txn for txn in txns if not(self.is_transaction_synced(txn)) ]
+    def filter(self, txns):
+        return [ txn for txn in txns if not(self.is_txn_synced(txn)) ]
+
+    def get_new_txns(self, acct):
+        days = 7
+        last_txns_len = 0
+        while (True):
+            raw = StringIO(acct.download(days))
+            ofx = OfxParser.parse(raw)
+            txns = ofx.account.statement.transactions
+            new_txns = self.filter(txns)
+            if (last_txns_len == len(txns)):
+                # not getting anything new; we have reached the beginning
+                return new_txns
+            elif (len(txns) > len(new_txns)):
+                # got more txns than were new, we've reached a stopping point
+                return new_txns
+            else:
+                # all new txns, increase how far back we go
+                days = days * 2
+                last_txns_len = len(txns)
--- tests/test_sync.py
+++ tests/test_sync.py
@@ -1,10 +1,12 @@
 import os
 import os.path
 from ofxparse import OfxParser
+from ofxclient.config import OfxConfig
 from ledgerautosync.ledger import Ledger
 from ledgerautosync.sync import Synchronizer
 
 from unittest import TestCase
+from mock import Mock
 
 class TestLedger(TestCase):
     def test_fresh_sync(self):
@@ -12,11 +14,26 @@ class TestLedger(TestCase):
         ofx = OfxParser.parse(file('fixtures/checking.ofx'))
         sync = Synchronizer(ledger)
         txns = ofx.account.statement.transactions
-        self.assertEqual(sync.sync(txns), txns)
+        self.assertEqual(sync.filter(txns), txns)
 
     def test_fully_synced(self):
         ledger = Ledger("fixtures/checking.lgr")
         ofx = OfxParser.parse(file('fixtures/checking.ofx'))
         sync = Synchronizer(ledger)
         txns = ofx.account.statement.transactions
-        self.assertEqual(sync.sync(txns), [])
+        self.assertEqual(sync.filter(txns), [])
+
+    def test_no_new_txns(self):
+        ledger = Ledger("fixtures/checking.lgr")
+        acct = Mock()
+        acct.download = Mock(return_value=file('fixtures/checking.ofx').read())
+        sync = Synchronizer(ledger)
+        self.assertEqual(len(sync.get_new_txns(acct)), 0)
+        
+    def test_all_new_txns(self):
+        ledger = Ledger("fixtures/empty.lgr")
+        acct = Mock()
+        acct.download = Mock(return_value=file('fixtures/checking.ofx').read())
+        sync = Synchronizer(ledger)
+        self.assertEqual(len(sync.get_new_txns(acct)), 3)
+        
