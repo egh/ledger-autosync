82cc0ec98e6e53cf4853a3304d75cf4c8622ef46
Erik Hetzner <egh@e6h.org>
2013-08-16 08:09:37 -0700

    add investment formatting

--- ledgerautosync/formatter.py
+++ ledgerautosync/formatter.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-
+from ofxparse.ofxparse import Transaction, InvestmentTransaction
 class Formatter(object):
     def __init__(self, acctid, currency, name):
         self.currency = currency
@@ -9,19 +9,30 @@ class Formatter(object):
     def mk_dynamic_account(self, txn):
         return "Expenses:Misc"
     
-    def format_amount(self, txn, reverse=False):
+    def format_amount(self, amount, reverse=False):
         currency = self.currency.upper()
         if currency == "USD": currency = "$"
-        amt = "%0.2f"%(abs(txn.amount))
-        if txn.amount.is_signed() != reverse:
+        amt = "%0.2f"%(abs(amount))
+        if amount.is_signed() != reverse:
             return "-%s%s"%(currency, amt)
         else:
             return "%s%s"%(currency, amt)
-            
+
     def format_txn(self, txn):
-        date = "%s"%(txn.date.strftime("%Y/%m/%d"))
-        retval = "%s %s\n"%(date, txn.memo)
-        retval += "  ; ofxid: %s.%s\n"%(self.acctid, txn.id)
-        retval += "  %s  %s\n"%(self.name, self.format_amount(txn))
-        retval += "  %s  %s\n"%(self.mk_dynamic_account(txn), self.format_amount(txn, True))
+        retval = ""
+        if isinstance(txn, Transaction):
+            date = "%s"%(txn.date.strftime("%Y/%m/%d"))
+            retval += "%s %s\n"%(date, txn.memo)
+            retval += "  ; ofxid: %s.%s\n"%(self.acctid, txn.id)
+            retval += "  %s  %s\n"%(self.name, self.format_amount(txn.amount))
+            retval += "  %s  %s\n"%(self.mk_dynamic_account(txn), self.format_amount(txn.amount, True))
+        elif isinstance(txn, InvestmentTransaction):
+            trade_date = "%s"%(txn.tradeDate.strftime("%Y/%m/%d"))
+            if txn.settleDate is not None:
+                retval = "%s=%s %s\n"%(txn.tradeDate, txn.settleDate.strftime("%Y/%m/%d"), txn.memo)
+            else:
+                retval = "%s %s\n"%(txn.tradeDate, txn.memo)
+            retval += "  ; ofxid: %s.%s\n"%(self.acctid, txn.id)
+            retval += "  %s  %s %s @ %s\n"%(self.name, txn.units, txn.security, txn.unit_price)
+            retval += "  %s  %s\n"%(self.name, self.format_amount(txn.units * txn.unit_price, True))
         return retval
