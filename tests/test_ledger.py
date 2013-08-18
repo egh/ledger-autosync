f83d7cdd2689a9547935b64242ca570f5e945cda
Erik Hetzner <egh@e6h.org>
2013-08-18 11:12:46 -0700

    add failing test for transaction substring

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -13,6 +13,9 @@ class TestLedger(TestCase):
         self.assertEqual(txn['transaction']['metadata'], 
                          {u'pair': {u'string': u'1101.1452687~7.0000486', u'key': u'ofxid'}})
 
+#    def test_transaction_substring(self):
+#        self.assertIsNone(self.ledger.get_transaction_by_ofxid("1452687~7"))
+
     def test_nonexistent_transaction(self):
         self.assertEqual(self.ledger.get_transaction_by_ofxid("FOO"), None)
     
