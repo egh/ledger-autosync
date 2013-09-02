36c349d51e8f38f6e7c941615c3e5c11d9ab80a5
Erik Hetzner <egh@e6h.org>
2013-09-02 09:24:09 -0700

    test on all 3 possible ledger processes

--- tests/test_ledger.py
+++ tests/test_ledger.py
@@ -6,42 +6,33 @@ from unittest import TestCase
 
 class TestLedger(TestCase):
     def setUp(self):
-        self.ledger = Ledger("fixtures/checking.lgr")
-        self.hledger = HLedger("fixtures/checking.lgr")
-        self.ledger_nopipe = Ledger("fixtures/checking.lgr", no_pipe=True)
-
+        self.ledgers = [Ledger("fixtures/checking.lgr"),
+                        HLedger("fixtures/checking.lgr"),
+                        Ledger("fixtures/checking.lgr", no_pipe=True)]
     def test_transaction(self):
-        self.assertTrue(self.ledger.check_transaction_by_ofxid("1101.1452687~7.0000486"))
-        self.assertTrue(self.hledger.check_transaction_by_ofxid("1101.1452687~7.0000486"))
-        self.assertTrue(self.ledger_nopipe.check_transaction_by_ofxid("1101.1452687~7.0000486"))
+        for ledger in self.ledgers:
+            self.assertTrue(ledger.check_transaction_by_ofxid("1101.1452687~7.0000486"))
         
 #    def test_transaction_substring(self):
 #        self.assertIsNone(self.ledger.get_transaction_by_ofxid("1452687~7"))
 
     def test_nonexistent_transaction(self):
-        self.assertFalse(self.ledger.check_transaction_by_ofxid("FOO"))
-        self.assertFalse(self.hledger.check_transaction_by_ofxid("FOO"))
-        self.assertFalse(self.ledger_nopipe.check_transaction_by_ofxid("FOO"))
+        for ledger in self.ledgers:
+            self.assertFalse(ledger.check_transaction_by_ofxid("FOO"))
     
     def test_get_account_by_payee(self):
-        account = self.ledger.get_account_by_payee("AUTOMATIC WITHDRAWAL, ELECTRIC BILL WEB(S )", exclude="Assets:Foo")
-        self.assertEqual(account, "Expenses:Bar")
-
-        account = self.hledger.get_account_by_payee("AUTOMATIC WITHDRAWAL, ELECTRIC BILL WEB(S )", exclude="Assets:Foo")
-        self.assertEqual(account, "Expenses:Bar")
+        for ledger in self.ledgers:
+            account = ledger.get_account_by_payee("AUTOMATIC WITHDRAWAL, ELECTRIC BILL WEB(S )", exclude="Assets:Foo")
+            self.assertEqual(account, "Expenses:Bar", msg="%s != Expenses:Bar with %s"%(account, ledger))
 
     def test_get_ambiguous_account_by_payee(self):
-        ledger = Ledger("fixtures/checking-dynamic-account.lgr")
-        hledger = HLedger("fixtures/checking-dynamic-account.lgr")
-
-        account = ledger.get_account_by_payee("Generic", exclude="Assets:Foo")
-        self.assertEqual(account, None)
-
-        account = hledger.get_account_by_payee("Generic", exclude="Assets:Foo")
-        self.assertEqual(account, None)
-
-        account = ledger.get_account_by_payee("Generic", exclude="Assets:Foo")
-        self.assertEqual(account, None)
+        ledgers = [Ledger("fixtures/checking-dynamic-account.lgr"),
+                   Ledger("fixtures/checking-dynamic-account.lgr", no_pipe=True),
+                   HLedger("fixtures/checking-dynamic-account.lgr")]
+        
+        for ledger in ledgers:
+            account = ledger.get_account_by_payee("Generic", exclude="Assets:Foo")
+            self.assertEqual(account, None)
                                                                                                              
     # broken in ledger
 #    def test_multiple_transaction(self):
