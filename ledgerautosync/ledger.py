7f61f11bde9dca4396fcc46961118c73c05839bf
Simon Michael <simon@joyful.com>
2014-01-13 17:10:01 -0800

    fix account selection with hledger
    We increase the register output width so we are likely to get the full
    unelided account names, up to 80 characters or so. This means we now
    require hledger 0.20+.

--- ledgerautosync/ledger.py
+++ ledgerautosync/ledger.py
@@ -139,7 +139,7 @@ class HLedger(object):
         return self.run(cmd) != ''
 
     def get_account_by_payee(self, payee, exclude):
-        cmd = ["reg", "desc:%s"%(payee)]
+        cmd = ["reg", "-w200", "desc:%s"%(payee)]
         lines = self.run(cmd).splitlines()
-        accts = [ l[32:59].strip() for l in lines ]
+        accts = [ l[92:172].strip() for l in lines ]
         return all_or_none([ a for a in accts if a != exclude ])
