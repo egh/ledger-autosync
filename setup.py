ec9cad64c3e677e1dc47b484c340a974504ef526
Erik Hetzner <egh@e6h.org>
2016-07-18 22:08:03 -0700

    bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='ledger-autosync',
-      version="0.2.4",
+      version="0.2.5",
       description="Automatically sync your bank's data with ledger",
       long_description=long_description,
       classifiers=[
