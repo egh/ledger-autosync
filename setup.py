7a303f3a693261d10f677c01fb08f35c105a1e1b
Erik Hetzner <egh@e6h.org>
2019-04-14 18:33:07 -0700

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='ledger-autosync',
-    version="1.0.0",
+    version="1.0.1",
     description="Automatically sync your bank's data with ledger",
     long_description=long_description,
     author='Erik Hetzner',
