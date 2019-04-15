9922d3ee57e6f6c7cda27d1978c5eb9f9b0e2b37
Erik Hetzner <egh@e6h.org>
2019-04-14 18:21:36 -0700

    Bump requirements

--- setup.py
+++ setup.py
@@ -38,8 +38,7 @@ setup(
     install_requires=[
         'setuptools>=26',
         'ofxclient',
-        'ofxparse>=0.16',
-        'BeautifulSoup4'
+        'ofxparse'
     ],
 
     extras_require={
